5e4bc7440db9e4f1bdfde8ae1f105190c52fcb88
vathsalaraju <92009074+vathsalaraju@users.noreply.github.com>
2022-04-05 20:12:19 +0530

    Update DashboardController.cs
    Added Account Details and Account  Summary

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -4,9 +4,32 @@ namespace BankingManagementSystem.Controllers
 {
     public class DashboardController : Controller
     {
+    private readonly VSFBankContext _db;
+        public DashboardController(VSFBankContext context)
+        {
+            _db = context;
+        }
+        
         public IActionResult Index()
         {
+        //TempData["Accountnumber"] = 500102300208;
             return View();
+        }
+        public IActionResult AccountSummary()
+        {
+            //List<CustomerAcc> customerAccs = _db.CustomerAccs.ToList();
+            var result = (from details in _db.CustomerAccs
+                          where details.CustomerId == "1001"
+                          select details).ToList();
+            return View(result);
+        }
+        public IActionResult AccountDetails()
+        {
+            //List<TransactionDetail> transactionDetails = _db.TransactionDetails.ToList();
+            var result = (from details in _db.TransactionDetails
+                          where details.AccountNumber == 200106300808
+                          select details).ToList();
+            return View(result);
         }
     }
 }
